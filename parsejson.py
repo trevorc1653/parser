55fa04bf11fe96f0c9af04f611f1590f36f0d29f
daragon-devnet <daragon@sdccd.edu>
2020-12-20 23:38:58 +0000

    Final module 3 lab

---
+++ parsejson.py
@@ -0,0 +1,13 @@
+# Fill in this file with the code from parsing JSON exercise
+import json
+import yaml
+
+with open('myfile.json','r') as json_file:
+    ourjson = json.load(json_file)
+
+print(ourjson)
+
+print("\n\n","-"*40)
+print(yaml.dump(ourjson))
+
+
